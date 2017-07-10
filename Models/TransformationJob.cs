c61ed091fb8146b9c265b56d4afb975467de4092
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2017-07-10 21:58:10 +0200

    Adding TransformationJob model
    --HG--
    branch : dev

---
+++ Models/TransformationJob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hast.Remote.Bridge.Models.Api;
+
+namespace Hast.Remote.Bridge.Models
+{
+    public class TransformationJob : TransformationRequest
+    {
+        public int UserId { get; set; }
+    }
+}
