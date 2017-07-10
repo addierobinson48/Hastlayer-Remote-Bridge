ab7af511a79d232177abd705fd07ad220b8bb348
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2017-07-10 23:27:42 +0200

    Adding TransformationJobResult
    --HG--
    branch : dev

---
+++ Models/TransformationJobResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hast.Remote.Bridge.Models
+{
+    public class TransformationJobResult : TransformationResult
+    {
+        public int UserId { get; set; }
+    }
+}
