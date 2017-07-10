84b824e152f9004175469a11c9dab6219703dc07
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2017-07-10 20:39:29 +0200

    Adding some more API models for transformation
    --HG--
    branch : dev

---
+++ Models/Api/TransformationRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hast.Remote.Bridge.Models.Api
+{
+    public class TransformationRequest
+    {
+        public HardwareGenerationConfiguration Configuration { get; set; }
+        public IEnumerable<AssemblyContainer> Assemblies { get; set; }
+    }
+}
---
+++ Models/Api/TransformationTicket.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hast.Remote.Bridge.Models.Api
+{
+    public class TransformationTicket
+    {
+        public string Token { get; set; }
+    }
+}
